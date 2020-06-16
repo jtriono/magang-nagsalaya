4cf2354bf49059e3e48f90f09325c24440503932
jeff <jtriono@student.ciputra.ac.id>
2020-06-16 15:53:49 +0700

    konek database

--- nagsa/nagsa/Formlogin.vb
+++ nagsa/nagsa/Formlogin.vb
@@ -1,18 +1,35 @@
-﻿Public Class Formlogin
-
+﻿Imports MySql.Data.MySqlClient
+Public Class Formlogin
+    Dim conn As New MySqlConnection("server=localhost;uid=root;pwd=;database=apotik")
+    Dim comm As New MySqlCommand
+    Dim adapt As New MySqlDataAdapter
+    Dim query As String
+    Dim tampunguser As New DataTable
     Private Sub btlogin_Click(sender As Object, e As EventArgs) Handles btlogin.Click
-        If TextBoxuser.Text <> "MOKO" Or TextBoxpass.Text <> "1234" Then
-            MessageBox.Show("Username atau password salah")
-        Else
+        query = "select username,password from userid where username='" + TextBoxuser.Text + "'"
+        comm = New MySqlCommand(query, conn)
+        adapt = New MySqlDataAdapter(comm)
+        adapt.Fill(tampunguser)
+        If TextBoxuser.Text = tampunguser.Rows(0).Item(0).ToString And TextBoxpass.Text = tampunguser.Rows(0).Item(1).ToString Then
             Me.Visible = False
             TextBoxuser.Text = ""
             TextBoxpass.Text = ""
             Formutama.ShowDialog()
+        Else
+            MessageBox.Show("username atau password salah")
         End If
-
+        tampunguser.Clear()
     End Sub
 
     Private Sub btkeluar_Click(sender As Object, e As EventArgs) Handles btkeluar.Click
         Close()
+    End Sub
+
+
+    Private Sub TextBoxpass_KeyUp(sender As Object, e As KeyEventArgs) Handles TextBoxpass.KeyUp
+        If e.KeyCode = Keys.Enter Then
+            btlogin.PerformClick()
+        End If
+
     End Sub
 End Class
--- nagsa/nagsa/Formutama.Designer.vb
+++ nagsa/nagsa/Formutama.Designer.vb
@@ -28,8 +28,6 @@ Partial Class Formutama
         Me.btnsupplier = New System.Windows.Forms.Button()
         Me.btncustomer = New System.Windows.Forms.Button()
         Me.dtpwaktu = New System.Windows.Forms.DateTimePicker()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
         Me.btnbarang = New System.Windows.Forms.Button()
         Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
         Me.KToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
@@ -148,24 +146,6 @@ Partial Class Formutama
         Me.dtpwaktu.Size = New System.Drawing.Size(116, 20)
         Me.dtpwaktu.TabIndex = 5
         Me.dtpwaktu.Value = New Date(2020, 6, 9, 10, 44, 0, 0)
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(320, 333)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(77, 13)
-        Me.Label1.TabIndex = 7
-        Me.Label1.Text = "NAS Computer"
-        '
-        'Label2
-        '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(274, 366)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(187, 13)
-        Me.Label2.TabIndex = 8
-        Me.Label2.Text = "081-33333-7365 HP. 081-7033-13512"
         '
         'btnbarang
         '
@@ -566,8 +546,6 @@ Partial Class Formutama
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(722, 398)
         Me.Controls.Add(Me.btnbarang)
-        Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.dtpwaktu)
         Me.Controls.Add(Me.btncustomer)
         Me.Controls.Add(Me.btnsupplier)
@@ -590,8 +568,6 @@ Partial Class Formutama
     Friend WithEvents btnsupplier As System.Windows.Forms.Button
     Friend WithEvents btncustomer As System.Windows.Forms.Button
     Friend WithEvents dtpwaktu As System.Windows.Forms.DateTimePicker
-    Friend WithEvents Label1 As System.Windows.Forms.Label
-    Friend WithEvents Label2 As System.Windows.Forms.Label
     Friend WithEvents btnbarang As System.Windows.Forms.Button
     Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
     Friend WithEvents KToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
