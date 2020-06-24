7bb1a45f5d61f3266bec516226a816662b08eb0e
jeff <jtriono@student.ciputra.ac.id>
2020-06-24 16:26:05 +0700

    barang

--- nagsa/nagsa/Barang.Designer.vb
+++ nagsa/nagsa/Barang.Designer.vb
@@ -22,16 +22,53 @@ Partial Class Formbarang
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.dgvbarang = New System.Windows.Forms.DataGridView()
+        Me.bthapus = New System.Windows.Forms.Button()
+        Me.bttambah = New System.Windows.Forms.Button()
+        CType(Me.dgvbarang, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'dgvbarang
+        '
+        Me.dgvbarang.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvbarang.Location = New System.Drawing.Point(12, 24)
+        Me.dgvbarang.Name = "dgvbarang"
+        Me.dgvbarang.Size = New System.Drawing.Size(736, 287)
+        Me.dgvbarang.TabIndex = 5
+        '
+        'bthapus
+        '
+        Me.bthapus.Location = New System.Drawing.Point(93, 317)
+        Me.bthapus.Name = "bthapus"
+        Me.bthapus.Size = New System.Drawing.Size(75, 23)
+        Me.bthapus.TabIndex = 4
+        Me.bthapus.Text = "Hapus"
+        Me.bthapus.UseVisualStyleBackColor = True
+        '
+        'bttambah
+        '
+        Me.bttambah.Location = New System.Drawing.Point(12, 317)
+        Me.bttambah.Name = "bttambah"
+        Me.bttambah.Size = New System.Drawing.Size(75, 23)
+        Me.bttambah.TabIndex = 3
+        Me.bttambah.Text = "Tambah"
+        Me.bttambah.UseVisualStyleBackColor = True
         '
         'Formbarang
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.ClientSize = New System.Drawing.Size(783, 370)
+        Me.Controls.Add(Me.dgvbarang)
+        Me.Controls.Add(Me.bthapus)
+        Me.Controls.Add(Me.bttambah)
         Me.Name = "Formbarang"
         Me.Text = "Barang"
+        CType(Me.dgvbarang, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
+    Friend WithEvents dgvbarang As System.Windows.Forms.DataGridView
+    Friend WithEvents bthapus As System.Windows.Forms.Button
+    Friend WithEvents bttambah As System.Windows.Forms.Button
 End Class
--- nagsa/nagsa/Barang.vb
+++ nagsa/nagsa/Barang.vb
@@ -1,3 +1,20 @@
-﻿Public Class Formbarang
+﻿Imports MySql.Data.MySqlClient
+Public Class Formbarang
+    Dim conn As New MySqlConnection("server=localhost;uid=root;pwd=;database=apotik")
+    Dim comm As New MySqlCommand
+    Dim adapt As New MySqlDataAdapter
+    Dim query As String
+    Dim tampungbarang As New DataTable
+    Private Sub Formbarang_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        tampungbarang.Clear()
+        query = "select * from barang"
+        comm = New MySqlCommand(query, conn)
+        adapt = New MySqlDataAdapter(comm)
+        adapt.Fill(tampungbarang)
+        dgvbarang.DataSource = tampungbarang
+    End Sub
 
+    Private Sub bttambah_Click(sender As Object, e As EventArgs) Handles bttambah.Click
+        Formtambahbarang.ShowDialog()
+    End Sub
 End Class
