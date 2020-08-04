0d1e73237d5d7b2fd41f11c6b32d1df0e968664f
jeff <jtriono@student.ciputra.ac.id>
2020-08-04 14:25:01 +0700

    perbaiki

--- nagsa/nagsa/Nominal_bayar_piutang.vb
+++ nagsa/nagsa/Nominal_bayar_piutang.vb
@@ -18,7 +18,7 @@ Public Class Nominal_bayar_piutang
 
     Private Sub btnbayar_Click(sender As Object, e As EventArgs) Handles btnbayar.Click
         'Try
-        '    query = "update piutang set lunas = 1, tanggal_pelunasan = '" + Form_Bayar_Piutang.dtptanggal.Value.ToString + "' where total_harga = '" + Form_Bayar_Piutang.simpanharga + "' and lunas = 0"
+        '    query = "update piutang set lunas = 1, tanggal_transaksi = '" + Form_Bayar_Piutang.dtptanggal.Value.ToString + "' where total_harga = '" + Form_Bayar_Piutang.simpanharga + "' and lunas = 0"
         '    connect.Open()
         '    command = New MySqlCommand(query, connect)
         '    command.ExecuteNonQuery()
@@ -27,40 +27,32 @@ Public Class Nominal_bayar_piutang
         '    MsgBox(ex.Message)
         '    connect.Close()
         'End Try
-        Try
-            If CInt(tbharga.Text) - CInt(tbinputbayar.Text) = 0 Then
-                query = "update piutang set lunas = 1, total_harga = 0 where total_harga = '" + Form_Bayar_Piutang.simpanharga + "' and lunas = 0"
-            ElseIf CInt(tbharga.Text) - CInt(tbinputbayar.Text) < 0 Then
-                MessageBox.Show("Input Salah")
-            ElseIf CInt(tbharga.Text) - CInt(tbinputbayar.Text) > 0 Then
-                query = "update piutang set total_harga = '" + harga + "' where total_harga = '" + Form_Bayar_Piutang.simpanharga + "' and lunas = 0"
-            End If
-            connect.Open()
-            command = New MySqlCommand(query, connect)
-            command.ExecuteNonQuery()
-            connect.Close()
-        Catch ex As Exception
-            connect.Close()
-        End Try
 
-        Try
-            query = "insert into bayar_piutang Values('" + tbnota.Text + "','" + tbnama.Text + "', sum(total_bayar + ('" + tbinputbayar.Text + "')), '" + Form_Bayar_Piutang.dtptanggal.ToString + "',0"
+        If CInt(tbharga.Text) - CInt(tbinputbayar.Text) = 0 Then
+            query = "update piutang set lunas = 1, total_harga = 0 where total_harga = '" + Form_Bayar_Piutang.simpanharga + "' and lunas = 0"
+        ElseIf CInt(tbharga.Text) - CInt(tbinputbayar.Text) < 0 Then
+            MessageBox.Show("Input Salah")
+        ElseIf CInt(tbharga.Text) - CInt(tbinputbayar.Text) > 0 Then
+            query = "update piutang set total_harga = '" + harga.ToString + "' where total_harga = '" + Form_Bayar_Piutang.simpanharga + "' and lunas = 0"
+        End If
+        connect.Open()
+        command = New MySqlCommand(query, connect)
+        command.ExecuteNonQuery()
+        connect.Close()
+
+
+        query = "insert into bayar_piutang Values('" + tbnota.Text + "','" + tbnama.Text + "', '" + tbinputbayar.Text + "', '" + Form_Bayar_Piutang.dtptanggal.Value.ToString + "',0)"
             connect.Open()
             command = New MySqlCommand(query, connect)
             command.ExecuteNonQuery()
             connect.Close()
-        Catch ex As Exception
-            connect.Close()
-        End Try
-        MessageBox.Show("Data Berhasil Masuk")
+            MessageBox.Show("Data Berhasil Masuk")
+        
+
     End Sub
 
     Private Sub tbinputbayar_TextChanged(sender As Object, e As EventArgs) Handles tbinputbayar.TextChanged
-        Try
-            harga = 0
-            harga = (CInt(tbharga.Text) - CInt(tbinputbayar.Text))
-        Catch ex As Exception
-
-        End Try
+        harga = 0
+        harga = (CInt(tbharga.Text) - CInt(tbinputbayar.Text))
     End Sub
 End Class
