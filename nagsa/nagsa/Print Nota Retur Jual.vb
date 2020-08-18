5e4ebdd428f53a19c290191d1ce782044586c43c
jeff <jtriono@student.ciputra.ac.id>
2020-08-18 13:25:38 +0700

    perbaiki print

--- nagsa/nagsa/Print Nota Retur Jual.vb
+++ nagsa/nagsa/Print Nota Retur Jual.vb
@@ -1,13 +1,13 @@
 ﻿Imports MySql.Data.MySqlClient
 Public Class Print_Nota_Retur_Jual
-    Dim conn As New MySqlConnection("server=localhost;uid=root;pwd=admin;database=apotik")
+    Dim conn As New MySqlConnection("server=localhost;uid=root;pwd=;database=apotik")
     Dim comm As New MySqlCommand
     Dim adapt As New MySqlDataAdapter
     Dim query As String
     Dim datevalue As String
 
     Private Sub Print_Nota_Retur_Jual_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        query = "select * from nota_retur_jual where `delete`= 0 and and retur = 1 no_retur_penjualan ='" + Retur_Penjualan.tbno_nota.Text.ToString + "'"
+        query = "select * from nota_retur_jual where `delete`= 0 and retur = 1 and no_retur_penjualan ='" + Retur_Penjualan.tbno_nota.Text.ToString + "'"
         conn.Open()
         comm = New MySqlCommand(query, conn)
         adapt.SelectCommand = comm
